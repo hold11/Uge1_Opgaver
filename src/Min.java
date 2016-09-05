8350681b19f9652b6dde4f1c6b9027fb869bf438
Anders W. Olsen <awo@defalt.net>
2016-09-05 15:12:21 +0200

    git ignore

--- src/Min.java
+++ src/Min.java
@@ -3,7 +3,8 @@
  * Opgave 01.2 Bonus - Mindste Tal (Simpel version)
  */
 
-// Skriv en klasse Min med den statiske metode min(double a, double b, double c), der returnerer det mindste tal. Extra Bonus: Skriv metoden uden {} og med kun ét semikolon.
+// Skriv en klasse Min med den statiske metode min(double a, double b, double c),
+// der returnerer det mindste tal. Extra Bonus: Skriv metoden uden {} og med kun ét semikolon.
 
 public class Min {
     public static double min (double a, double b, double c) {
